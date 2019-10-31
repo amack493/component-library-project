0306cbb8a970ab54fc0c9bdabc8c8307c0900c8f
Tre <amack4693@gmail.com>
2019-10-30 23:24:19 -0400

    created border button

---
+++ src/components/Button/Button.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Button.css'
+// This is a functional component - just sent up a little differently as an arrow function!
+const Button = (props) => {
+    let classList = ''
+    let types = ['primary', 'danger', 'success', 'warning', 'default']
+    if (types.includes(props.type)) {
+        classList += ` button-${props.type}`
+    }
+    if (props.large) {
+        classList += ` button-large`
+    }
+    if (props.border) {
+        classList += ` button-border`
+    }
+   return <button class={classList}>
+        {props.label}
+    </button>
+}
+
+export default Button;
---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Button from './Button';
+
+
+
+
+
+storiesOf('Button', module)
+    .add('Primary', () => <Button label="Primary" type="primary"/>)
+    .add('Danger', () => <Button label="Danger" type="danger"/>) 
+    .add('Success', () => <Button label="Success" type="success"/>)
+    .add('Warning', () => <Button label="Warning" type="warning"/>)
+    .add('Default', () => <Button label="Default" type="default"/>)
+    .add('Primary Border', () => <Button label="Primary Border" type="primary" border/>) 
+    .add('Danger Border', () => <Button label="Danger Border" type="danger" border red/>) 
+
+
+
+    // .add('Large Primary', () => <Button label="Large" type="primary" large/>)
+    // .add('Large Danger', () => <Button label="Large Danger" type="danger" large/>)
+    // .add('Success', () => <Button label="Success" type="success" outline/>)
